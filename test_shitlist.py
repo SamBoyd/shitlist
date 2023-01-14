762fff65801cb08fa2dd06dff375fc308063a9af
Sam Boyd <samboyd10@gmail.com>
2023-01-14 13:52:50 +0000

    Remove file throwing off pytest

--- test_shitlist.py
+++
@@ -1,8 +0,0 @@
-import ast
-
-from shitlist.deprecated_code_use_collector import DeprecatedCodeUseCollector
-
-collector = DeprecatedCodeUseCollector(deprecated_code='', modulename='shitlist', package='shitlist')
-
-with open('tests/example_file.py', 'r') as f:
-    collector.visit(ast.parse(f.read()))
